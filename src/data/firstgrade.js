ed1d2710ae51f9f2c5ffb44f65bf833b0af24184
DahyeonKang <kl1223k@naver.com>
2020-09-02 14:32:42 +0900

    CS Base part

---
+++ src/data/firstgrade.js
@@ -0,0 +1,52 @@
+export const firstgradeCollection = {
+	name: "1학년",
+	areas: [
+		{
+			name: "1학기",
+			skills: [
+				{
+					name: "미래설계탐색",
+					description: {
+						text: "대학생활 전주기적인 관점에서 진로 및 취업 설계를 입학 직후부터 체계적으로 수립할 수 있는 능력을 배양한다.",
+						links: [
+							["Youtube - Video for your future","https://youtu.be/Xk87UNf6hW0"]
+						]
+					}
+				},
+				{
+					name: "창의공학설계",
+					description: {
+						text: "개개인의 창의력과 팀 기반의 프로젝트 수행을 바탕으로 다양한 공학주제에 대한 설계, 제작 경험을 통하여 실제 시스템 구현 원리에 대한 이해와 응용기술 등을 습득한다.",
+						links: [
+							["Invensis - Project Team ", "https://www.invensislearning.com/articles/pmp/what-is-a-project-team-and-who-all-are-involved"]
+						]
+					}
+				}
+			]
+		},
+		{
+			name: "2학기",
+			skills: [
+				{
+					name: "미래설계준비",
+					description: {
+						text: "대학생활 전주기적인 관점에서 진로 및 취업 설계를 저학년에서부터 체계적으로 수립할 수 있는 능력을 배양한다."
+					}
+				},
+				{
+					name: "오픈소스SW이해 및 실습",
+					description: {
+						text: "오픈소스 소프트웨어의 종류 및 활용사례에 대해 배우고, Git/Github과 같은 관리 시스템을 기반으로 버전 관리, 개발 및 문서화에 대한 실습을 진행한다.",
+						links: [
+							["GitHerb Guides", "https://guides.github.com/activities/hello-world/"],
+							["Blog - Git for beginners", "https://nolboo.kim/blog/2013/10/06/github-for-beginner/"],
+							["Github.io - Using open source correctly","https://naver.github.io/OpenSourceGuide/book/UsingOss/the-legal-side-of-opensource.html#licensedef"],
+							["MDN - Git and GitHub", "https://developer.mozilla.org/ko/docs/Learn/Tools_and_testing/GitHub"],
+							["Version Management", "https://git-scm.com/book/ko/v2/%EC%8B%9C%EC%9E%91%ED%95%98%EA%B8%B0-%EB%B2%84%EC%A0%84-%EA%B4%80%EB%A6%AC%EB%9E%80%3F"]
+						]
+					}
+				}
+			]
+		}		
+	]
+}
