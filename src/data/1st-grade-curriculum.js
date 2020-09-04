75f06f9fb38e37550784747923a05e58c9b322e4
sjhyun131 <aodlfendb131@gmail.com>
2020-09-05 02:47:50 +0900

    이미지 수정 #43

--- src/data/1st-grade-curriculum.js
+++ src/data/1st-grade-curriculum.js
@@ -50,8 +50,8 @@ export const firstgradeCollection = {
 							["Version Management", "https://git-scm.com/book/ko/v2/%EC%8B%9C%EC%9E%91%ED%95%98%EA%B8%B0-%EB%B2%84%EC%A0%84-%EA%B4%80%EB%A6%AC%EB%9E%80%3F"]
 						]
 					},
-				}
+				},
 			]
-		}		
+		},		
 	]
 }
