4a9a1ba0a327e4ccf153e0b357f2dc93ca0e5614
younjeong99 <jjung1819@naver.com>
2020-09-04 22:48:20 +0900

    1,2,3,4학년 전체코드 merge 및 4학년 코드 수정

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ import {secondgradeCollection} from "./data/2nd-grade-curriculum.js";
 import {firstgradeCollection} from "./data/1st-grade-curriculum.js";
 import {webcomponentsCollection} from "./data/3th-grade-curriculum.js";
 import {accessibilityCollection} from "./data/accessibility.js";
-import {pwaCollection} from "./data/4th grade curriculum.js";
+import {pwaCollection} from "./data/4th-grade-curriculum.js";
 import {architectureCollection} from "./data/architecture-and-paradigmes.js";
 import {buildtoolsCollection} from "./data/build-tools.js";
 import {testingCollection} from "./data/testing.js";
