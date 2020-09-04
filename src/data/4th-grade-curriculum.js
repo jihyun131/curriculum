405b11c9bd7be6fbe0157ed01069e3d79a608f2b
sjhyun131 <aodlfendb131@gmail.com>
2020-09-04 20:01:18 +0900

    폴더 수정 (#25)

--- src/data/4th-grade-curriculum.js
+++ src/data/4th-grade-curriculum.js
@@ -1,8 +1,8 @@
 export const pwaCollection = {
-   name: "4th-grade-curriculum",
+   name: "4th grade curriculum",
    areas: [
       {
-         name: "1st semester-required",
+         name: "1st semester_required",
          skills: [
             {
                name: "캡스톤디자인",
@@ -17,7 +17,7 @@ export const pwaCollection = {
       },
 
       {
-         name: "1st semester-eledive",
+         name: "1st semester_elective",
          skills: [
             {
                 name: "머신러닝",
@@ -112,7 +112,7 @@ export const pwaCollection = {
       },
 
       {
-         name: "2nd semester-eledive",
+         name: "2nd semester_elecdive",
          skills: [
             {
                name: "인공신경망",
